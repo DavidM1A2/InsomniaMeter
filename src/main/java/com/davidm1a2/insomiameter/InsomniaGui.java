1ccb9aa0aaec27b34bff6abd5c73a527d99996a6
David Slovikosky <davidslovikosky@gmail.com>
2020-05-26 20:19:41 -0700

    Add insomnia level bar

--- src/main/java/com/davidm1a2/insomiameter/InsomniaGui.java
+++ src/main/java/com/davidm1a2/insomiameter/InsomniaGui.java
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.gui.AbstractGui;
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.network.play.ClientPlayNetHandler;
 import net.minecraft.network.play.client.CClientStatusPacket;
 import net.minecraft.stats.Stats;
@@ -13,16 +12,14 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-import java.awt.*;
-
 /**
  * GUI object that shows a player's insomnia level
  */
 @OnlyIn(Dist.CLIENT)
 public class InsomniaGui extends AbstractGui
 {
-    // The insomnia background texture
-    private static final ResourceLocation TEXTURE = new ResourceLocation(Constants.MOD_ID, "textures/gui/insomnia_texture.png");
+    // The insomnia bar texture
+    private static final ResourceLocation BAR_TEXTURE = new ResourceLocation(Constants.MOD_ID, "textures/gui/bars.png");
     // How long it takes for phantoms to spawn
     private static final int TIME_WHEN_PHANTOMS_CAN_SPAWN = 72000;
     // A reference to the MC instance
@@ -65,31 +62,31 @@ public class InsomniaGui extends AbstractGui
 
             // Enable transparency
             RenderSystem.enableBlend();
-            // Bind the background texture
-            minecraft.textureManager.bindTexture(TEXTURE);
             // Compute the x and y of the texture
             final int width = this.minecraft.getMainWindow().getScaledWidth();
             final int height = this.minecraft.getMainWindow().getScaledHeight();
-            final int x = width / 2 - 8;
-            final int y = height - 54;
+            final int x = width / 2 - 98;
+            final int y = height - 38;
             // Compute the ticks since the last rest, use the server's sent value + the number of ticks that have passed on the client
             final int ticksSinceLastRest = player.getStats().getValue(Stats.CUSTOM.get(Stats.TIME_SINCE_REST)) + timeSinceLastServerUpdate;
-            // Compute the alpha of the background
-            final float alpha = computeAlpha(ticksSinceLastRest);
-            // Use a white color with the correct alpha
-            RenderSystem.color4f(1.0f, 1.0f, 1.0f, alpha);
-            // Draw the texture
-            blit(x, y, 64, 64, 16, 16, 16, 16);
-            // Disable transparency
+            // Compute the percent until we can see phantoms
+            final float percent = computePercent(ticksSinceLastRest);
+            // Use a white color
+            RenderSystem.color4f(1.0f, 1.0f, 1.0f, 1.0f);
+            // Bind the background texture
+            minecraft.textureManager.bindTexture(BAR_TEXTURE);
+            // Draw the background texture
+            blit(x, y, 5, 0, 5, 37, 64, 64);
+            // Draw the foreground texture
+            if (percent == 1.0)
+            {
+                blit(x, y, 10, 0, 5, 37, 64, 64);
+            }
+            else
+            {
+                blit(x, y - Math.round(percent * 37) + 37, 0, 37 - Math.round(percent * 37), 5, Math.round(percent * 37), 64, 64);
+            }
             RenderSystem.disableBlend();
-
-            // Create a string containing the alpha value rounded to no decimal places
-            final String textPercent = String.format("%2.0f", alpha * 100);
-            final FontRenderer fontRenderer = minecraft.fontRenderer;
-            final int textWidth = fontRenderer.getStringWidth(textPercent);
-            // Render the percent to being able to spawn phantoms as text
-            fontRenderer.drawString(textPercent, x - textWidth / 2 + 8, y + 6, Color.WHITE.getRGB());
-
             RenderSystem.popMatrix();
         }
     }
@@ -100,7 +97,7 @@ public class InsomniaGui extends AbstractGui
      * @param timeSinceLastRest The number of ticks since the last time we slept
      * @return A value from 0 (invisible) to 1 (visible)
      */
-    private float computeAlpha(int timeSinceLastRest)
+    private float computePercent(int timeSinceLastRest)
     {
         return MathHelper.clamp((float) timeSinceLastRest / TIME_WHEN_PHANTOMS_CAN_SPAWN, 0.0f, 1.0f);
     }
